a8d770baa36321db2c9cb46979a719089006d209
BreakByBrick <119886555+BreakByBrick@users.noreply.github.com>
2024-04-08 22:27:15 +0300

    ImageToTextConverter

---
+++ ImageToTextConverter/ImageToTextConverter/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace ImageToTextConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(1, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Выбрать изображение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(1, 83);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(234, 51);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Конвертировать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1, 157);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(234, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1, 202);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(234, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(252, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Масштаб конвертирования:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(-2, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Путь к изображению:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(-2, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(179, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Путь к текстовому файлу:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(-2, 227);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Сообщения:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1, 247);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(752, 153);
+            this.textBox3.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(252, 73);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(206, 22);
+            this.textBox5.TabIndex = 14;
+            this.textBox5.Text = "5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(252, 18);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(376, 17);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "НАСТРОЙКА ПАРАМЕТРОВ КОНВЕРТИРОВАНИЯ";
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(249, 107);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(192, 27);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Символы:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(252, 134);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(206, 22);
+            this.textBox6.TabIndex = 17;
+            this.textBox6.Text = "@&$#?!*/;:,. ";
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(464, 44);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(289, 56);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "- Данный параметр задает количество пикселей изображения, отображаемых в ширине/в" +
+    "ысоте одного символа";
+            // 
+            // label10
+            // 
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(464, 107);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(289, 70);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "- Данный параметр содержит символы, которыми будут отображены цвета изображения в" +
+    " текстовом файле, в порядке возрастания яркости";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(-2, 18);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 17);
+            this.label11.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(760, 407);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Converter (bmp/jpg/png/gif)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ ImageToTextConverter/ImageToTextConverter/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ImageToTextConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Bitmap bmp;
+        int picture_width, picture_height;
+
+        bool isPictureChoosed = false;
+
+        //кнопка выбора изображения
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image files (*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                Image image = Image.FromFile(dialog.FileName);
+                picture_width = image.Width;
+                picture_height = image.Height;
+                bmp = new Bitmap(image, picture_width, picture_height);
+
+                isPictureChoosed = true;
+                textBox1.Text = dialog.FileName;
+
+                textBox3.Text = textBox3.Text + "Выбрано изображение - " + dialog.FileName + "." + Environment.NewLine;
+                textBox3.Text = textBox3.Text + "Ширина - " + Convert.ToString(bmp.Width)+ " пикселей." + Environment.NewLine;
+                textBox3.Text = textBox3.Text + "Высота - " + Convert.ToString(bmp.Height) + " пикселей." + Environment.NewLine;
+                textBox3.Text = textBox3.Text + Environment.NewLine;
+            }
+        }
+
+        //кнопка конвертирования
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (isPictureChoosed == false)
+            {
+                textBox3.Text = textBox3.Text + "Выберите изображение" + Environment.NewLine;
+                textBox3.Text = textBox3.Text + Environment.NewLine;
+            }
+            else if  (textBox5.Text == "")
+            {
+                textBox3.Text = textBox3.Text + "Введите значение параметра 'Масштаб конвертирования'." + Environment.NewLine;
+                textBox3.Text = textBox3.Text + Environment.NewLine;
+            }
+            else if (Convert.ToInt32(textBox5.Text) == 0)
+            {
+                textBox3.Text = textBox3.Text + "Неверно указано значение параметра 'Масштаб конвертирования'. минимальное значение - 1." + Environment.NewLine;
+                textBox3.Text = textBox3.Text + Environment.NewLine;
+            }
+            else if ((Convert.ToInt32(textBox5.Text) >bmp.Width / 2)||(Convert.ToInt32(textBox5.Text) > bmp.Height / 2))
+            {
+                textBox3.Text = textBox3.Text + "Слишком большое значение параметра 'Масштаб конвертирования' " + Environment.NewLine;
+                textBox3.Text = textBox3.Text + Environment.NewLine;
+            }
+            else if (textBox6.Text == "")
+            {
+                textBox3.Text = textBox3.Text + "Параметр символы должен содержать хотя бы один символ." + Environment.NewLine;
+                textBox3.Text = textBox3.Text + Environment.NewLine;
+            }
+            else
+            {
+                textBox3.Text = textBox3.Text + "Конвертирование...." + Environment.NewLine;
+                textBox3.Text = textBox3.Text + Environment.NewLine;
+
+                int pixels_for_symbol_width = Convert.ToInt32(Convert.ToDouble(Convert.ToInt32(textBox5.Text)) / (Convert.ToDouble(bmp.Height) / (Convert.ToDouble(bmp.Width) / 0.85)));
+                int pixels_for_symbol_height = Convert.ToInt32(textBox5.Text);
+
+                int number_of_symbols = textBox6.TextLength;
+                string [] symbols = new string[number_of_symbols];
+
+                for (int i = 0; i < number_of_symbols; i++)
+                {
+                    symbols[i] = Convert.ToString(textBox6.Text[i]);
+                }
+
+                SaveFileDialog savedialog = new SaveFileDialog();
+                savedialog.Title = "Сохранить картинку";
+                savedialog.OverwritePrompt = true;
+                savedialog.CheckPathExists = true;
+                savedialog.Filter = "Text File(*.TXT)|*.TXT";
+                if (savedialog.ShowDialog() == DialogResult.OK)
+                {
+                    textBox2.Text = savedialog.FileName;
+                    StreamWriter fin = new StreamWriter(savedialog.FileName, false, Encoding.Default);
+
+                    for (int i = 0; i < bmp.Height / pixels_for_symbol_height; i++)
+                    {
+                        for (int j = 0; j < bmp.Width / pixels_for_symbol_width; j++)
+                        {
+                            int avg_color_value = 0;
+                            for (int l = 0; l < pixels_for_symbol_width; l++)
+                            {
+                                for (int p = 0; p < pixels_for_symbol_height; p++)
+                                {
+                                    int RED_bright = bmp.GetPixel(j * pixels_for_symbol_width + l, i * pixels_for_symbol_height + p).R;
+                                    int GREEN_bright = bmp.GetPixel(j * pixels_for_symbol_width + l, i * pixels_for_symbol_height + p).G;
+                                    int BLUE_bright = bmp.GetPixel(j * pixels_for_symbol_width + l, i * pixels_for_symbol_height + p).B;
+                                    if ((l == 0) && (p == 0))
+                                        avg_color_value = (RED_bright + GREEN_bright + BLUE_bright) / 3;
+                                    else
+                                        avg_color_value = (avg_color_value + (RED_bright + GREEN_bright + BLUE_bright) / 3) / 2;
+                                }
+                            }
+
+
+                            int symbol_color = 255 / number_of_symbols;
+                            int k = 1;
+                            while (symbol_color * k < avg_color_value)
+                            {
+                                k++;
+                            }
+                            k--;
+                            k = Convert.ToInt32(Convert.ToDouble(k) * (Convert.ToDouble(symbols.Length) / Convert.ToDouble(number_of_symbols)));
+                            while (k > symbols.Length - 1)
+                                k--;
+                            fin.Write("{0,2}", symbols[k]);
+                        }
+                        fin.Write("\r\n");
+                    }
+                    fin.Close();
+                }
+
+                textBox3.Text = textBox3.Text + "Изображение успешно конвертировано в - " + savedialog.FileName + "." + Environment.NewLine;
+                textBox3.Text = textBox3.Text + Environment.NewLine;
+            }
+        }
+    }
+}
